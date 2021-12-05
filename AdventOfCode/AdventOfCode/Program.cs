359ce9a2bb0208604ad5c2b2220d49919d07eef3
David Hampton <DavidHampton@live.com>
2021-12-04 20:28:40 -0700

    Finished day 4. Decided not to be clever and just solve for the solution.

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -4,9 +4,11 @@ using AdventOfCode;
 // Actual result for advent of code 2021.
 //Day1Logic.AnalyzeDay1(@"TestFiles\day1.txt");
 //Day2Logic.AnalyzeDay2(@"TestFiles\day2.txt");
-Day3Logic.AnalyzeDay3(@"TestFiles\day3.txt");
+//Day3Logic.AnalyzeDay3(@"TestFiles\day3.txt");
+Day4Logic.AnalyzeDay4(@"TestFiles\day4.txt");
 
 // This is to assist me in testing the test data.
 //Day1Logic.AnalyzeDay1(@"TestFiles\day1TestData.txt");
 //Day2Logic.AnalyzeDay2(@"TestFiles\day2TestData.txt");
-Day3Logic.AnalyzeDay3(@"TestFiles\day3TestData.txt");
+//Day3Logic.AnalyzeDay3(@"TestFiles\day3TestData.txt");
+Day4Logic.AnalyzeDay4(@"TestFiles\day4TestData.txt");

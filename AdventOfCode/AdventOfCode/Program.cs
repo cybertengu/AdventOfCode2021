1438ae8b6773874d15442533153a5ba004f92c67
David Hampton <dhampton@macu.com>
2021-12-07 16:32:41 -0700

    Finished day 7.

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -7,7 +7,8 @@ using AdventOfCode;
 //Day3Logic.AnalyzeDay3(@"TestFiles\day3.txt");
 //Day4Logic.AnalyzeDay4(@"TestFiles\day4.txt");
 //Day5Logic.AnalyzeDay5(@"TestFiles\day5.txt");
-Day6Logic.AnalyzeDay6(@"TestFiles\day6.txt");
+//Day6Logic.AnalyzeDay6(@"TestFiles\day6.txt");
+Day7Logic.AnalyzeDay7(@"TestFiles\day7.txt");
 
 // This is to assist me in testing the test data.
 //Day1Logic.AnalyzeDay1(@"TestFiles\day1TestData.txt");
@@ -15,4 +16,5 @@ Day6Logic.AnalyzeDay6(@"TestFiles\day6.txt");
 //Day3Logic.AnalyzeDay3(@"TestFiles\day3TestData.txt");
 //Day4Logic.AnalyzeDay4(@"TestFiles\day4TestData.txt");
 //Day5Logic.AnalyzeDay5(@"TestFiles\day5TestData.txt");
-Day6Logic.AnalyzeDay6(@"TestFiles\day6TestData.txt");
+//Day6Logic.AnalyzeDay6(@"TestFiles\day6TestData.txt");
+Day7Logic.AnalyzeDay7(@"TestFiles\day7TestData.txt");

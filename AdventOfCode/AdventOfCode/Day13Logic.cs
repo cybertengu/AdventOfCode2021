af54cd0ba684d1cae40d853b45bd4dcc8e9f2d43
David Hampton <DavidHampton@live.com>
2021-12-13 07:13:14 -0700

    Updated logic to get me information I need to work with.

--- AdventOfCode/AdventOfCode/Day13Logic.cs
+++ AdventOfCode/AdventOfCode/Day13Logic.cs
@@ -5,15 +5,15 @@
         public static void AnalyzeDay(string fileName)
         {
             Console.WriteLine("Start of day 13");
-            AnalyzeDayA(fileName);
+            Day13Logic day13Logic = new Day13Logic();
+            day13Logic.AnalyzeDayA(fileName);
             Console.WriteLine("End of day 13");
         }
 
-        static void AnalyzeDayA(string fileName)
+        void AnalyzeDayA(string fileName)
         {
             List<string> lines = new List<string>();
             List<string> foldLines = new List<string>();
-            List<Utility.Position> positions = new List<Utility.Position>();
 
             using (StreamReader streamReader = new StreamReader(fileName))
             {
@@ -39,7 +39,36 @@
                 streamReader.Close();
             }
 
+            List<Utility.Position> inputs = new List<Utility.Position>();
+            foreach (string input in foldLines)
+            {
+                var tokens = input.Split(' ');
+                var coordinates = tokens[2].Split('=');
+                int x = Convert.ToInt32(coordinates[0]);
+                int y = Convert.ToInt32(coordinates[1]);
+                Utility.Position position = new Utility.Position(x, y);
+                inputs.Add(position);
+            }
 
+            Dictionary<int, List<int>> positions = new Dictionary<int, List<int>>();
+            foreach (var line in lines)
+            {
+                var tokens = line.Split(',');
+                var x = Convert.ToInt32(tokens[0]);
+                var y = Convert.ToInt32(tokens[1]);
+
+                if (positions.ContainsKey(x))
+                {
+                    positions[x].Add(y);
+                }
+                else
+                {
+                    positions.Add(x, new List<int> { y });
+                }
+            }
+
+            long numberOfDots = 0;
+            Console.WriteLine(numberOfDots);
         }
     }
 }
